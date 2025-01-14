b3de290a248e9ea1163ba140ad593c5d4a378ee8
logcow123 <logcow123@gmail.com>
2025-01-13 22:53:12 -0700

    did prep 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?");
+        string fname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"your name is {lname}, {fname} {lname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Please Enter your Score!");
+        int score = int.Parse(Console.ReadLine());
+        string letterGrade;
+
+        if(score >= 90)
+        {
+            letterGrade = "A";
+        } else if(score >= 80)
+        {
+            letterGrade = "B";
+        } else if(score >= 70)
+        {
+            letterGrade = "C";
+        } else if(score >= 60)
+        {
+            letterGrade = "D";
+        } else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your Grade is a {letterGrade}");
     }
+
 }
