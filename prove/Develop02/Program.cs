f7e2c495ae2ef379dfb443bbc0fa31b9595b4cb6
logcow123 <logcow123@gmail.com>
2025-01-30 13:37:34 -0700

    added menu system

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,19 +6,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
-        Entry myEntry1 = new Entry();
-        Entry myEntry2 = new Entry();
         Journal myJournal = new Journal();
+        Boolean continueWriting = true;
 
-        myEntry1.WriteEntry();
-        myEntry2.WriteEntry();
+        while(continueWriting)
+        {
+            Console.WriteLine("\n1. Write new entry");
+            Console.WriteLine("2. Display all entries");
+            Console.WriteLine("3. Display Specific Entry");
+            Console.WriteLine("4. Remove Specific Entry");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option (1-5): ");
+        
+            string choice = Console.ReadLine();
 
-        myJournal.AddEntry(myEntry1);
-        myJournal.AddEntry(myEntry2);
+            switch(choice)
+            {
+                case "1":
+                    Entry newEnrty = new Entry();
+                    newEnrty.WriteEntry();
+                    myJournal.AddEntry(newEnrty);
+                    break;
+                case "2":
+                    myJournal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter Entry Index: ");
+                    int entryIndex = int.Parse(Console.ReadLine());
+                    myJournal.GetEntry(entryIndex).ReadEntry();
+                    break;
+                case "4":
+                    Console.Write("Enter Entry Index: ");
+                    int removeIndex = int.Parse(Console.ReadLine());
+                    myJournal.RemoveEntry(myJournal.GetEntry(removeIndex));
+                    break;
+                case "5":
+                    continueWriting = false;
+                    break;
 
-        myJournal.DisplayEntries();
-        Console.WriteLine("---------GETTING ENTRY 0---------");
-        myJournal.GetEntry(0).ReadEntry();
+                default:
+                    break;
+            }
+
+        }
 
 
     }
