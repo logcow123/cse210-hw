64b48716daec6cf360f5d4fbf3c63ebeb4ae5d74
logcow123 <logcow123@gmail.com>
2025-02-06 16:39:21 -0700

    added a try except to the main loop and allowed the use to choose how many words will be removed

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -14,18 +14,31 @@ class Program
         while(myVerse.CheckAllHidden() == false)
         {
             
-            Console.WriteLine("Press 'ENTER to contine or type 'quit' to guit");
+            Console.WriteLine("Press 'ENTER to contine or type 'quit' to quit or type a number to remove that many words");
             userInput = Console.ReadLine();
             if(userInput == "quit")
             {
                 break;
-            }
-            myVerse.HideWords(7);
+            }else if(userInput != "")                       //This section of code checks if the input is an integer and if so, will hide that many words
+            {
+                try
+                {
+                    int userNum = int.Parse(userInput);
+                    myVerse.HideWords(userNum);
+                    Console.Clear();
+                    myVerse.DisplayVerse();
+                }catch(Exception e)                         //If the input cannot be parsed, it will still continue
+                {
+                    Console.Clear();
+                    myVerse.DisplayVerse();
+                    Console.WriteLine(e.Message);
+                }   
+            }else                //Default will be 3 words removed
+            {
+            myVerse.HideWords(3);
             Console.Clear();
             myVerse.DisplayVerse();
+            } 
         }
-
-
-        
     }
 }
